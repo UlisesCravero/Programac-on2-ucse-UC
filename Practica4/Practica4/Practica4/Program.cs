59edd99cf0c750520aa0353910d6e20ca0207429
Ulises Cravero <ulisescravero95@gmail.com>
2020-04-21 15:50:37 -0300

    no message

--- Practica4/Practica4/Practica4/Program.cs
+++ Practica4/Practica4/Practica4/Program.cs
@@ -7,11 +7,34 @@ using Logica;
 
 namespace Practica4
 {
+    public sealed class Palabra
+    {
+        public string elString { get; set; }
+    }
+
+    public static class Extension
+    {
+        public static int CantidadPalabras(string p)
+        {
+            int resultado = p.Split(' ').Length;
+            return resultado;
+        }
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            Palabra unaPalabra = new Palabra();
+            unaPalabra.elString = Console.ReadLine();
+            Console.WriteLine(Extension.CantidadPalabras(unaPalabra.elString));
 
+
+
+
+            Console.ReadLine();
         }
     }
 }
